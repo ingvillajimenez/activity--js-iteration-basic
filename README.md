fbba7397e542d409a9ccd39fa10741cab54dcc42
ingvillajimenez <ing.villa.jimenez@gmail.com>
2018-11-22 09:51:55 -0600

    warmup

---
+++ README.md
@@ -0,0 +1,13 @@
+# JS Iteration Basic Warmup
+
+### Setup
+
+```
+# (1) go to ~/muktek/warmups
+cd ~/muktek/warmups
+
+# (2) clone this repo
+git clone https://github.com/muktek/activity--js-iteration-basic.git
+
+```
+# activity--js-iteration-basic
---
+++ ex-01-hasQ.js
@@ -0,0 +1,37 @@
+//------------------------------------------------------------------
+// TASK : hasQ()
+//   Write a function called hasQ that accepts a string and returns true/false
+//   for whether that string has the word 'q' in it.
+
+//   NOTE: You must build the function so that it can detect capital letters
+//------------------------------------------------------------------
+
+function hasQ(string){
+  var value = 0;
+  for(var i = 0; i < string.length; i++){
+    if(string[i] == 'Q' || string[i] == 'q'){
+        value++;
+    }
+  }
+  if(value > 0){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+
+var output1 = hasQ("my mother enjoys quilting.")
+var output2 = hasQ("i'm not wearing any socks.")
+var output3 = hasQ("I went to a quarry to pick up some stone slabs.")
+var output4 = hasQ("Quit yelling in my ear.")
+var output5 = hasQ("My best friend's name is Quincy.")
+var output6 = hasQ("My brain is made of cheese and my knees are jelly")
+
+console.assert(output1 === true )
+console.assert(output2 === false )
+console.assert(output3 === true)
+console.assert(output4 === true)
+console.assert(output5 === true)
+console.assert(output6 === false)
